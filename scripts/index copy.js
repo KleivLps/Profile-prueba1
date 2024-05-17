af3a87fc5a24f76803d76dd9fcffa22cf210564a
kleiver <kleiver1604@gmail.com>
2024-05-16 20:27:31 -0500

    Initial commit

---
+++ scripts/index copy.js
@@ -0,0 +1,8 @@
+
+
+const numero1 = Number(prompt("ingrese primer numero"));
+const numero2 = Number(prompt("ingrese segundo numero"));
+
+const suma = numero1 + numero2;
+
+const mensajeSuma= "el resultado de la suma es " +suma ;
---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const selector = document.getElementById("mensaje");
+
+selector.innerHTML = "coneccion exitosa entre html y js";
+
+const nombre = prompt("podrias indicarme un nombre por favor");
+
+const mensaje = "bienvenido " + nombre;
+
+console.log("tu nombre es: " + nombre);
+alert("Biemvenido a mi pagina " + nombre + "!");
+
+
+let ciudadDeOrigen = "lima"
+
+const valordeEdad =27
+
+let nombres = "kleiver"
+let apellido ="chapoñan"
+
+let edad =27
+let AñodeNacimiento = 1996
+
+const tienehijos = false
+const estacasado = true
+
+const numero1 = Number(prompt(18));
+const numero2 = Number(prompt(20));
+
+const suma = numero1 + numero2;
+
+const mensajeSuma = "el resultado de la suma es "+suma;
